dbb1f36da7349d429955e64683a6c0f316fa190d
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2020-11-08 12:34:43 -0300

    Formatando a estrutura da aplicação e criando o componente funcional Header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from '../Header';
 import './App.css';
-import TestComponent from '../TestComponent';
-// import ClassComponent from '../ClassComponent';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <TestComponent name="Props name" />
-                    {/* <ClassComponent name="Porps name" /> */}
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <Header title="AlgStock" />
+                App
             </div>
         );
     }
