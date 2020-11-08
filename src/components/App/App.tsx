9ccfd46d634ea0f6775595832d05a6cb334540ff
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2020-11-08 16:47:40 -0300

    Utilizando os props dos componentes funcionais

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import Button from '../Button';
 import Header from '../Header';
 import './App.css';
+
+function TestComponent() {
+
+    return <img 
+        width="16"
+        src="https://img.icons8.com/pastel-glyph/2x/search--v2.png"
+        alt="Search icon"
+    />
+} 
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Header title="AlgStock" />
-                App
+                <Header title="AlgaStock" />
+
+                <div className="Container">
+                    <Button 
+                        content="Click me" 
+                        onClick={ () => window.alert('Click') }
+                        appendIcon={ <TestComponent /> }
+                    >
+                        Alert
+                    </Button>
+                </div>
             </div>
         );
     }
